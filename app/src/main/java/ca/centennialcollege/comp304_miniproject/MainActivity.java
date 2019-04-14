fc9bdbc780c4bd4d0c5427efa278d781a172bb8f
eduardogodoy <eduardo.ewgo@gmail.com>
2019-04-13 21:55:07 -0400

    Basic cleaning - might use the generic methods in the map.

--- app/src/main/java/ca/centennialcollege/comp304_miniproject/MainActivity.java
+++ app/src/main/java/ca/centennialcollege/comp304_miniproject/MainActivity.java
@@ -22,38 +22,26 @@ import java.net.URLConnection;
 
 import ca.centennialcollege.comp304_miniproject.models.DataRepository;
 import ca.centennialcollege.comp304_miniproject.services.MusicService;
+import ca.centennialcollege.comp304_miniproject.utils.ExternalAsset;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    ImageView img;
-
     final private int REQUEST_INTERNET = 123;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initial data
         DataRepository.SeedData();
 
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.INTERNET)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.INTERNET},
-                    REQUEST_INTERNET);
-
+        // Verify permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, REQUEST_INTERNET);
         } else {
-
-
-            // for text
-            // new DownloadTextTask().execute("http://jfdimarzio.com/test.htm");
-            //for image
             new DownloadImageTask().execute("https://www.bdpinternational.com/uploads/attachments/cj8nd0rnt003ks6qpbpxxru4a-port-cover-image.0.218.4288.2412.max.jpg");
-            // for web services
-            //new AccessWebServiceTask().execute("Logistics");
         }
-
     }
 
     public void btnStartService_onClick(View view) {
@@ -64,49 +52,9 @@ public class MainActivity extends AppCompatActivity {
         stopService(new Intent(getBaseContext(), MusicService.class));
     }
 
-    private InputStream OpenHttpConnection(String urlString) throws IOException {
-        InputStream in = null;
-        int response = -1;
-
-        URL url = new URL(urlString);
-        URLConnection conn = url.openConnection();
-
-        if (!(conn instanceof HttpURLConnection))
-            throw new IOException("Not an HTTP connection");
-        try {
-            HttpURLConnection httpConn = (HttpURLConnection) conn;
-            httpConn.setAllowUserInteraction(false);
-            httpConn.setInstanceFollowRedirects(true);
-            httpConn.setRequestMethod("GET");
-            httpConn.connect();
-            response = httpConn.getResponseCode();
-            if (response == HttpURLConnection.HTTP_OK) {
-                in = httpConn.getInputStream();
-            }
-        } catch (Exception ex) {
-            Log.d("Networking", ex.getLocalizedMessage());
-            throw new IOException("Error connecting");
-        }
-        return in;
-    }
-
-    private Bitmap DownloadImage(String URL) {
-        Bitmap bitmap = null;
-        InputStream in = null;
-        try {
-            in = OpenHttpConnection(URL);
-            bitmap = BitmapFactory.decodeStream(in);
-            in.close();
-        } catch (IOException e1) {
-            Log.d("NetworkingActivity", e1.getLocalizedMessage());
-        }
-        return bitmap;
-    }
-
-
     private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
         protected Bitmap doInBackground(String... urls) {
-            return DownloadImage(urls[0]);
+            return ExternalAsset.DownloadImage(urls[0]);
         }
 
         protected void onPostExecute(Bitmap result) {
