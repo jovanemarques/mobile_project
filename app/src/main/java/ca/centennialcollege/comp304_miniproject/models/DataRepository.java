0365dea788094d31ac7394301743a6980deaf846
Ailton Lima <ailtonclima@gmail.com>
2019-04-13 21:05:57 -0400

    Finished the DataRepository

--- app/src/main/java/ca/centennialcollege/comp304_miniproject/models/DataRepository.java
+++ app/src/main/java/ca/centennialcollege/comp304_miniproject/models/DataRepository.java
@@ -10,8 +10,8 @@ public class DataRepository {
 
     private static DataRepository dataRepository;
     private static Dictionary<Integer, Client> clients = new Hashtable<>();
-    private static List<Order> orders = new ArrayList<>();
-    private static List<Deliverer> deliverers = new ArrayList<>();
+    private static Dictionary<Integer, Order> orders = new Hashtable<>();
+    private static Dictionary<Integer, Deliverer> deliverers = new Hashtable<>();
 
     public static void SeedData() {
 
@@ -19,18 +19,7 @@ public class DataRepository {
 
         populateOrders();
 
-//        Product product1 = new Product();
-//        product1.setId(1);
-//        product1.setName("Smartphone");
-//
-//        products.add(product1);
-//
-//        Product product2 = new Product();
-//        product2.setId(2);
-//        product2.setName("Laptop");
-//
-//        products.add(product2);
-
+        populateDeliverers();
     }
 
     private static void populateClients() {
@@ -64,13 +53,61 @@ public class DataRepository {
         order.setId(1);
         order.setNumber(123456);
         order.setClient(clients.get(1));
-        order.addProduct(new Product("", 0.0f));
-        order.addProduct(new Product("", 0.0f));
+        order.addProduct(new Product("Pro ASP.NET Core MVC 2", 47.42f));
+        order.addProduct(new Product("Bluetooth Headphones, Parasom A1 Magnetic", 26.99f));
+        order.setStatus(OrderStatus.RECEIVED);
+        order.setDeliveryAddress(new Address(826, "Huntingwood Dr", "",
+                "Scarborough", "ON", "M1T 2L6", 43.794348f, -79.296368f));
 
-        orders.add(order);
+        orders.put(order.getId(), order);
+
+        order.setId(2);
+        order.setNumber(234567);
+        order.setClient(clients.get(2));
+        order.addProduct(new Product("Echo Dot (3rd gen) - Smart speaker with Alexa - Charcoal", 69.99f));
+        order.setStatus(OrderStatus.RECEIVED);
+        order.setDeliveryAddress(new Address(3760, "Sheppard Ave E", "Apt. 224",
+                "Scarborough", "ON", "M1T 3K9", 43.781942f, -79.294326f));
+
+        orders.put(order.getId(), order);
+
+        order.setId(3);
+        order.setNumber(345875);
+        order.setClient(clients.get(3));
+        order.addProduct(new Product("Cuisinart DGB-900BCC Automatic Burr Grind & Brew Thermal TM 12 Cup Coffeemaker", 174.99f));
+        order.setStatus(OrderStatus.ASSIGNED_TO_DELIVERER);
+        order.setDeliveryAddress(new Address(26, "Wardencourt Dr.", "",
+                "Scarborough", "ON", "M1T 2H3", 43.783300f, -79.307182f));
+
+        orders.put(order.getId(), order);
+
+        order.setId(4);
+        order.setNumber(345875);
+        order.setClient(clients.get(4));
+        order.addProduct(new Product("Asus Transformer Book T101HA-C4-GR 10.1-Inch 2 in 1 Touchscreen Laptop", 383.21f));
+        order.setStatus(OrderStatus.ASSIGNED_TO_DELIVERER);
+        order.setDeliveryAddress(new Address(38, "Eaglestone Rd", "",
+                "Scarborough", "ON", "M1T 2J6", 43.783954f, -79.304524f));
+
+        orders.put(order.getId(), order);
     }
 
+    private static void populateDeliverers() {
+        Deliverer deliverer = new Deliverer();
 
+        deliverer.setId(1);
+        deliverer.setName("John Doe");
+        deliverer.addOrder(orders.get(3));
+        deliverer.setCurrentLat(43.790505f);
+        deliverer.setCurrentLng(-79.302339f);
+
+
+        deliverer.setId(1);
+        deliverer.setName("Jane Smith");
+        deliverer.addOrder(orders.get(4));
+        deliverer.setCurrentLat(43.776807f);
+        deliverer.setCurrentLng(-79.277744f);
+    }
 
     public static Enumeration<Client> getClients() {
         return clients.elements();
@@ -80,15 +117,15 @@ public class DataRepository {
         clients.put(client.getId(), client);
     }
 
-    public static List<Order> getOrders() {
-        return orders;
+    public static Enumeration<Order> getOrders() {
+        return orders.elements();
     }
 
     public static void addOrder(Order order) {
-        orders.add(order);
+        orders.put(order.getId(), order);
     }
 
-    public static List<Deliverer> getDeliverers() {
-        return deliverers;
+    public static Enumeration<Deliverer> getDeliverers() {
+        return deliverers.elements();
     }
 }
